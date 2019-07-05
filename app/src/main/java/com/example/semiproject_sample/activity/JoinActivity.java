0c7b087599b2e3a03e21d3e7e530ac39a5320d97
KimJiYeon <kwldusrnclf@naver.com>
2019-07-05 12:07:42 +0900

    login, join xml과 자바파일 기초적인 코딩 + 카메라 사용을 위한 매니페스트 선언

--- app/src/main/java/com/example/semiproject_sample/activity/JoinActivity.java
+++ app/src/main/java/com/example/semiproject_sample/activity/JoinActivity.java
@@ -2,15 +2,56 @@ package com.example.semiproject_sample.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.example.semiproject_sample.R;
 
 public class JoinActivity extends AppCompatActivity {
 
+    //멤버변수
+    private ImageView mimgProfile;
+    private EditText medtId, medtName, medtPw1, medtPw2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join);
+
+        mimgProfile = findViewById(R.id.imgProfile);
+        medtId = findViewById(R.id.edtId);
+        medtName = findViewById(R.id.edtName);
+        medtPw1 = findViewById(R.id.edtPw1);
+        medtPw2 = findViewById(R.id.edtPw2);
+
+        //카메라 버튼
+        findViewById(R.id.btnCamera).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                captureCamera();
+            }
+        });
+
+        //회원가입 버튼
+        findViewById(R.id.btnJoin).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                joinPrecess();
+            }
+        });
+    }  //end onCreate()
+
+    //카메라 작업 시작
+    private void captureCamera(){
+
     }
-}
+
+    //회원가입 작업 시작
+    private void joinPrecess(){
+
+    }
+
+}  //end Class
--- app/src/main/java/com/example/semiproject_sample/activity/LoginActivity.java
+++ app/src/main/java/com/example/semiproject_sample/activity/LoginActivity.java
@@ -2,15 +2,53 @@ package com.example.semiproject_sample.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.semiproject_sample.R;
 
+//여기서 this = LoginActivity = Context = getBaseContext()
 public class LoginActivity extends AppCompatActivity {
+
+    //멤버 변수 선언
+    private EditText medtId, medtPw;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    }
+
+        medtId = findViewById(R.id.edtId);
+        medtPw = findViewById(R.id.edtPw);
+        Button btnLogin = findViewById(R.id.btnLogin);
+        Button btnJoin = findViewById(R.id.btnJoin);
+
+        btnLogin.setOnClickListener(mBtnLoginClick);
+        btnJoin.setOnClickListener(mBtnJoinClick);
+        //여기서 this = LoginActivity = Context = getBaseContext()
+
+
+    } //end onCreate()
+
+    //로그인 버튼 클릭 이벤트
+    private View.OnClickListener mBtnLoginClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+        }
+    };
+
+    //회원가입 버튼 클릭 이벤트
+    private View.OnClickListener mBtnJoinClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent i = new Intent(LoginActivity.this, JoinActivity.class);
+            startActivity(i);
+            //여기서 this = mBtnJoinClick > 왜냐면 new로 선언함으로써 mBtnJoinClick이 익명 클래스가 됨 > 그래서 LoginActivity를 this로 쓰고 싶으면 LoginActivity.this로 써야 함
+        }
+    };
 }
