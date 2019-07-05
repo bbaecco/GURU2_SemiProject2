70fe31ae6bae5d3c9cb4a8de5097bd0b41478a31
KimJiYeon <kwldusrnclf@naver.com>
2019-07-05 10:58:30 +0900

    Initial commit

---
+++ app/src/main/java/com/example/semiproject_sample/fragment/FragmentMemoWrite.java
@@ -0,0 +1,25 @@
+package com.example.semiproject_sample.fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.semiproject_sample.R;
+
+
+public class FragmentMemoWrite extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_memo_write, container, false);
+    }
+
+}
